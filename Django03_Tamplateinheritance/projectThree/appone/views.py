26c27c539794d4b192305d17f3d801146db4f366
swati <swatilingshetty@gmail.com>
2024-07-21 11:32:21 +0530

    Add files via upload

---
+++ Django03_Tamplateinheritance/projectThree/appone/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+# from django.template import loader
+
+# Create your views here.
+def about(request):
+    
+    return render(request,'about.html')
+
+def contact(request):
+    
+    return render(request,'contact.html')
+
+def home(request):
+   
+
+    return render(request,'home.html')
+
+def services(request):
+    
+    return render(request,'services.html')
+
